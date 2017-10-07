d757ab11128fa0363f4507b4ddc8926491faa603
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-10-07 11:03:48 +0900

    Update gitterframe.php

--- widgets/views/gitterframe.php
+++ widgets/views/gitterframe.php
@@ -4,11 +4,6 @@ use yii\helpers\Url;
 use humhub\models\Setting;
 
 ?>
-<div class="panel">
-  <div class="panel-heading">
-    <?= Yii::t('base', '<strong>Gitter</strong> Chat'); ?>
-  </div>
-  <div class="panel-body">
 
 <code>Replace This Line</code>
 
