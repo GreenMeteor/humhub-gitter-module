8a386ac50e49df97ca21053ab5814e2b75070e56
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-10-08 14:53:52 +0900

    Update gitterframe.php

--- widgets/views/gitterframe.php
+++ widgets/views/gitterframe.php
@@ -7,5 +7,9 @@ use humhub\models\Setting;
 
 <code>Replace This Line</code>
 
+<button class="js-gitter-toggle-chat-button">Toggle Chat</button>
+<button class="js-gitter-toggle-chat-button" data-gitter-toggle-chat-state="true">Open Chat</button>
+<button class="js-gitter-toggle-chat-button" data-gitter-toggle-chat-state="false">Close Chat</button>
+
 </div>
 </div>
